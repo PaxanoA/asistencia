7cd39167363856e88e548859e248fa1ea383ed9c
PaxanoA <72090162+PaxanoA@users.noreply.github.com>
2021-04-01 13:35:18 -0500

    Create code.js

---
+++ code.js
@@ -0,0 +1,32 @@
+
+let cantidad = prompt("cuantos alumnos son?");
+let alumnosTotales = [];
+
+for (i = 0; i < cantidad; i++) {
+	alumnosTotales[i] = [prompt("Nombre del alumno " + (i++)), 0];
+
+}
+
+const tomarAsistencia = (nombre, p) =>{
+	let presencia = prompt(nombre);
+	if (presencia == "p" || "P"){
+		alumnosTotales[p][1];
+	}
+}
+for (i = 0; i < 30; i++) {
+	for (alumno in alumnosTotales) {
+		tomarAsistencia(alumnosTotales[alumno][0]);
+	}
+}
+
+
+for (alumno in alumnosTotales) {
+	let resultado = `${alumnosTotales[alumno][0]}: </br>
+	______Presentes: ${alumnosTotales[alumno][1]} </br>
+	______Ausentes: ${30 - alumnosTotales[alumno][1]} </br>`
+	if (30 - alumnosTotales[alumno][1] > 18) {
+		resultado = "REPROBADO </br>"
+	}else{
+		resultado += "</br>"
+	}document.write(resultado);
+}
